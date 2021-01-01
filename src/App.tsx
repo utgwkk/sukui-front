d257ade94256b18694674b9c062e98f6bb00e0c0
utgwkk <utagawakiki@gmail.com>
2021-01-02 00:04:25 +0900

    loading button

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,32 @@ function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [images, setImages] = useState<Image[]>([]);
 
-  useEffect(() => {
+  const loadImages = (maxId?: number) => {
+    const url = maxId !== undefined ? `${apiImagesURL}?max_id=${maxId}` : apiImagesURL;
     axios
-      .get<ApiImagesResponse>(apiImagesURL)
+      .get<ApiImagesResponse>(url)
       .then((resp) => {
         setIsLoaded(true);
         setImages(resp.data.data);
       });
+  };
+
+  // initial loading
+  useEffect(() => {
+    loadImages();
   }, []);
 
+  const handleLoadImageButtonClick = () => {
+    setIsLoaded(false);
+    const maxId = images.length > 0 ? images[images.length - 1].id : undefined;
+    loadImages(maxId);
+  };
 
   return (
     <div className="App">
       <h1>It works!</h1>
       <ImageList isLoaded={isLoaded} images={images} />
+      <button onClick={handleLoadImageButtonClick}>画像を読み込む</button>
     </div>
   );
 }
